1863a68952c4457d9f49aebcd9103bc1b9494cc5
monkey.D Luffy <602110567@qq.com>
2021-04-13 16:45:18 +0800

    test

--- application/admin/controller/Garlic.php
+++ application/admin/controller/Garlic.php
@@ -18,26 +18,26 @@
 			    $red_6_5['title'] = '杂交红蒜';
                 $red_6_5['level'] = '6.5cm';
                 $garlic['red_6_5']= Db::table('51garlic')
-                    ->order('id asc')
+                    ->order('id desc')
                     ->where($red_6_5)
                     ->select();
                 $red_6_0['title'] = '杂交红蒜';
                 $red_6_0['level'] = '6.0cm';
                 $garlic['red_6_0']= Db::table('51garlic')
-                    ->order('id asc')
+                    ->order('id desc')
                     ->where($red_6_0)
                     ->select();
 
                 $red_5_5['title'] = '杂交红蒜';
                 $red_5_5['level'] = '5.5cm';
                 $garlic['red_5_5']= Db::table('51garlic')
-                    ->order('id asc')
+                    ->order('id desc')
                     ->where($red_5_5)
                     ->select();
                 $red_5_0['title'] = '杂交红蒜';
                 $red_5_0['level'] = '5.0cm';
                 $garlic['red_5_0']= Db::table('51garlic')
-                    ->order('id asc')
+                    ->order('id desc')
                     ->where($red_5_0)
                     ->select();
 
